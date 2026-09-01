e377ba7461061c83580b9008bfe599a926f3fb49
A-Brusnitsyn <al.brusnitsyn@yandex.ru>
2026-09-01 20:55:54 +0500

    Spotless added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("io.github.ben-manes.versions") version "0.61.0"
+    id("com.diffplug.spotless") version "8.10.1"
     application
 }
 
@@ -13,11 +14,25 @@ repositories {
 application {
     mainClass = "hexlet.code.App"
 }
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+spotless {
+    java {
+        importOrder()
+        removeUnusedImports()
+        googleJavaFormat().aosp()
+        formatAnnotations()
+        leadingTabsToSpaces(4)
+        endWithNewline()
+    }
 }
 
 tasks.test {
